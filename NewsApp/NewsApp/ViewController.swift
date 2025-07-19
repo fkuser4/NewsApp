06878a0bd627952c08a45fea7370a8052d4e563c
Filip Kušer <filipkuser@192.168.1.11>
2025-07-19 16:56:00 +0200

    Inital setup and storyboard cleanup

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Filip Kušer on 19.07.2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+}
